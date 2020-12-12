bc2d14f1c67fba6a73d524f524e821bd349def5c
kaaax0815 <999999bst@gmail.com>
2020-12-12 22:08:28 +0100

    finished weather gui

---
+++ Weather GUI/Select.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Weather_GUI
+{
+    /// <summary>
+    /// Interaktionslogik für Select.xaml
+    /// </summary>
+    public partial class Select : Window
+    {
+        public Select()
+        {
+            InitializeComponent();
+        }
+
+        private void Current_Click(object sender, RoutedEventArgs e)
+        {
+            Weather_Now w = new Weather_Now();
+            w.Show();
+        }
+
+        private void Forecast_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = new MainWindow();
+            w.Show();
+        }
+    }
+}
